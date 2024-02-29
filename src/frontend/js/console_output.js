9b0fc919e6bdd118b087637e0979bd55599b5392
Brian Meermans <brianmeermans@AoPS-HQ-OPT-BMeermans.local>
2024-02-29 10:23:24 -0800

    Small adjustments

--- src/frontend/js/console_output.js
+++ src/frontend/js/console_output.js
@@ -3,9 +3,12 @@ const logOutput = document.getElementById('log-output');
 const originalConsoleLog = console.log;
 
 console.log = function(message) {
+
     originalConsoleLog.apply(console, arguments);
 
     if (message !== 'Jar is loaded, main is starting' && message != 'CheerpJ runtime ready') {
         logOutput.value += message;
     }
+
+    logOutput.scrollTop = logOutput.scrollHeight;
 };
--- src/main/java/CustomInputStream.java
+++ src/main/java/CustomInputStream.java
@@ -2,7 +2,7 @@ import java.io.InputStream;
 import java.io.IOException;
 
 public class CustomInputStream extends InputStream {
-    private StringBuilder buffer = new StringBuilder();
+    private final StringBuilder buffer = new StringBuilder();
     private int position = 0;
 
     @Override
@@ -22,29 +22,9 @@ public class CustomInputStream extends InputStream {
         String data = getSystemIn();
 
         if (data != null && !data.isEmpty()) {
-            System.out.println(data);
             buffer.append(data + "\n\n");
         }
     }
-
-
-//    private int dummyIndex = 0;
-//    private String[] dummyData = {
-//            "1234",
-//            "3.14",
-//            "true",
-//            "hello",
-//            "this is four inputs"
-//    };
-//    public String getSystemIn() {
-//        if (dummyIndex < dummyData.length) {
-//            return dummyData[dummyIndex++];
-//        }
-//        return "";
-//
-//    }
-
-
 
     public static native String getSystemIn();
 }
